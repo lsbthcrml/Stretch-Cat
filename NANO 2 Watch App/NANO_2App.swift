1691d2c8100fb88bd0dca8906a4f474cbd2a3d24
Carmel <elisabethcarmela.appdev@gmail.com>
2024-05-17 11:19:58 +0700

    Initial Commit

---
+++ NANO 2 Watch App/NANO_2App.swift
@@ -0,0 +1,17 @@
+//
+//  NANO_2App.swift
+//  NANO 2 Watch App
+//
+//  Created by Elisabeth Carmela on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct NANO_2_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
